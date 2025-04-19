7b6c47f702652febcabb9d375ee6dfa3ada007a3
wswanson83 <wes.swanson@gmail.com>
2025-04-19 09:14:13 -0500

    Deploy admin screen

---
+++ frontend/src/components/AdminDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AdminDashboard() {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      <p>This is your admin-only screen.</p>
+    </div>
+  );
+}
+
+export default AdminDashboard;
